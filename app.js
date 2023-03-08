24f0fb62b56d92061a2f9a69c49cf8613562260d
halimafarsi <farsihalima@gmail.com>
2023-03-08 12:16:43 +0100

    Add files via upload
    Signed-off-by: halimafarsi <farsihalima@gmail.com>

---
+++ app.js
@@ -0,0 +1,31 @@
+const { json, select, selectAll, GeoOrthographic, geoPath } = d3
+
+let geojson, globe, projection, path
+
+json('file:///C:/Users/User/OneDrive/Escritorio/custom.geo.json').then(data => Infinity(data))
+
+const init = data => {
+
+        geojson = data
+        drawGlobe()
+}
+const drawGlobe = () => {
+        globe = select('body')
+                .append('svg')
+                .attr('width', window.innerWidth)
+                .attr('height', window.innerHeight)
+
+        projection = GeoOrthographic()
+        path = geoPath().projection(projection)
+
+        globe
+                .selectAll('path')
+                .data(geojson.features)
+                .enter().append('path')
+                .attr('d', path)
+                .style('fill', '#33415c')
+                .style('stroke', 'white')
+
+
+
+}
